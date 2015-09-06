b7d9ff6626a14303815c9b50481467ef93d32427
David256 <davidborja456@gmail.com>
2015-09-05 23:42:15 -0500

    se permite que la clase de accion pueda conocer que JMenuIten existen

--- src/acciones/menuAccion.java
+++ src/acciones/menuAccion.java
@@ -1,5 +1,7 @@
 package acciones;
 
+import interfaz.Ventana;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -17,7 +19,7 @@ public class menuAccion implements ActionListener, ItemListener{
 	@Override
 	public void actionPerformed(ActionEvent elEvent) {
 		// TODO Auto-generated method stub
-		if(elEvent.getSource() == menuItem){
+		if(elEvent.getSource() == Ventana.menuISalir){
 			System.exit(0);
 		}
 	}
