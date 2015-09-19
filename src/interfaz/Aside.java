e34103d9f7308b68becf2cfb15049960386b4d07
David256 <davidborja456@gmail.com>
2015-09-19 07:46:18 -0500

    Se mejoró la clase Aside

--- src/interfaz/Aside.java
+++ src/interfaz/Aside.java
@@ -1,15 +1,35 @@
 package interfaz;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class Aside extends JPanel{
-
+	
+	
+	private int X;
+	private int Y;
+	private int ancho;
+	private int alto;
+	
+	
+	public Aside(){
+		setBackground(new Color(210,210,210));
+	}
+	
+	
 	public void ubicar(int x, int y) {
 		// TODO Auto-generated method stub
+		X = x;
+		Y = y;
 		//calculamos la proporcion aurea para el ancho y el 95% para la altura
 		int anchoPerfecto =(int)( x - (x * 0.618) );
 		int altoPerfecto = (int) (y * 0.95);
 		setSize(anchoPerfecto, altoPerfecto);
+		
+		ancho = anchoPerfecto;
+		alto = altoPerfecto;
 		
 		int desplazamientoX = x - anchoPerfecto;
 		int desplazamientoY = 0;
