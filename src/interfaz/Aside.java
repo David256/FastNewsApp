669f50fc0c7062f67224e4cca1f2e37c3448aad7
David256 <davidborja456@gmail.com>
2015-09-19 07:20:36 -0500

    Implementación del método ubicar() ya definido

--- src/interfaz/Aside.java
+++ src/interfaz/Aside.java
@@ -6,7 +6,14 @@ public class Aside extends JPanel{
 
 	public void ubicar(int x, int y) {
 		// TODO Auto-generated method stub
+		//calculamos la proporcion aurea para el ancho y el 95% para la altura
+		int anchoPerfecto =(int)( x - (x * 0.618) );
+		int altoPerfecto = (int) (y * 0.95);
+		setSize(anchoPerfecto, altoPerfecto);
 		
+		int desplazamientoX = x - anchoPerfecto;
+		int desplazamientoY = 0;
+		setLocation(desplazamientoX, desplazamientoY);
 	}
 
 }
