b81cdd5011690cfbea1c34b2e444a98cdb5b13c0
David256 <davidborja456@gmail.com>
2015-10-25 10:32:53 -0500

    Actualizacion de libreria

---
+++ src/Canal.java
@@ -0,0 +1,8 @@
+//esta será una clase de datos, eso espero
+
+
+public class Canal {
+	private String url;
+	private String titulo;
+	
+}
--- src/FastNews.java
+++ src/FastNews.java
@@ -10,7 +10,7 @@ import interfaz.Ventana;
 public class FastNews {
 	
 	static Ventana ventana;
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ventana = new Ventana(); //este objeto maneja la ventana
