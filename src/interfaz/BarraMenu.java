3cf4df02d04801d4d1b593a32f170fae5909e0ed
David256 <davidborja456@gmail.com>
2015-10-25 01:01:09 -0500

    comentarios

--- src/interfaz/BarraMenu.java
+++ src/interfaz/BarraMenu.java
@@ -1,7 +1,17 @@
 package interfaz;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.JMenuBar;
 
 public class BarraMenu extends JMenuBar{
+	
+	public void paint(Graphics g){
+		g.setColor(new Color(00,23,23));
+		super.paint(g);
+		System.out.println("Mejoro el menu");
+	}
+
 
 }
