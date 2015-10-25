dae144bd1ac8a2a625e909a27938f3d0eb99e6f3
David256 <davidborja456@gmail.com>
2015-10-25 10:00:26 -0500

    Mejora estetica de ListaCanales

--- src/interfaz/ListaCanales.java
+++ src/interfaz/ListaCanales.java
@@ -1,5 +1,6 @@
 package interfaz;
 
+import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JList;
@@ -12,7 +13,11 @@ public class ListaCanales extends JList{
 		super(valores);
 		this.setSize(350, 400);
 		this.setLocation(8, 50);
-		this.setFont(new Font("Georgia", Font.BOLD, 24));
+		this.setFont(new Font(Font.SANS_SERIF,Font.CENTER_BASELINE , 18));
+		this.setForeground(new Color(107, 72, 1));
+		this.setBackground(new Color(203, 198, 176));
+		this.setSelectionBackground(new Color(0,0,0));
+		this.setSelectionForeground(Color.white);
 	}
 
 }
