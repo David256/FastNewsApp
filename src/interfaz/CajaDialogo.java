939a5d470c9946192011f912461c5b9a81263efb
David256 <davidborja456@gmail.com>
2015-10-27 21:42:21 -0500

    Cambio en el cuadro de dialogo y su funcion de agregar RSS

--- src/interfaz/CajaDialogo.java
+++ src/interfaz/CajaDialogo.java
@@ -2,6 +2,8 @@ package interfaz;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,11 +13,16 @@ import javax.swing.JTextField;
 
 
 
-public class CajaDialogo extends JFrame{
+public class CajaDialogo extends JFrame implements ActionListener{
 	
 	JPanel panel = new JPanel();
+	//normal
 	JLabel titulo = new JLabel();
-	JButton boton = new JButton();
+	JButton [] boton;
+	JLabel [] texto;
+	JTextField [] entrada; 
+	//no más
+	String espera = "";
 	
 	public CajaDialogo(){
 		this.setSize(400, 500);
@@ -26,41 +33,50 @@ public class CajaDialogo extends JFrame{
 	
 	//funcion que agrega canales
 	public void agregarCanal() {
+		//configuramos la ventana
+		espera = "addRSS";
+		boton = new JButton[1];
+		texto = new JLabel[2];
+		entrada = new JTextField[2];
+		
 		this.setSize(400, 300);
 		
 		//creamos objetos necesarios
-		JButton agregar = new JButton("Agregar");
-		agregar.setBackground(new Color(100,100,100));
-		agregar.setForeground(Color.white);
-		agregar.setBounds(125, 220, 100, 30); //corregir esto
+		boton[0] = new JButton("Agregar");
+		boton[0].setBackground(new Color(100,100,100));
+		boton[0].setForeground(Color.white);
+		boton[0].setBounds(150, 220, 100, 30); //corregir esto
 		
 		//agregamos campos de entrada de URL, titulo personalizado, y por ahora ya
-		JLabel lblUrl = new JLabel("URL: ");
-		JLabel lblTitulo = new JLabel("titulo: ");
-		lblUrl.setBounds(10, 50, 100, 30);
-		lblUrl.setFont(new Font("Arial", Font.BOLD, 15));
-		lblUrl.setForeground(Color.WHITE);
+		texto[0] = new JLabel("URL: ");
+		texto[1] = new JLabel("titulo: ");
+		texto[0].setBounds(10, 50, 100, 30);
+		texto[0].setFont(new Font("Arial", Font.BOLD, 15));
+		texto[0].setForeground(Color.WHITE);
 		
-		lblTitulo.setBounds(10, 90, 100, 30);
-		lblTitulo.setForeground(Color.WHITE);
-		lblTitulo.setFont(new Font("Arial", Font.BOLD, 15));
+		texto[1].setBounds(10, 90, 100, 30);
+		texto[1].setForeground(Color.WHITE);
+		texto[1].setFont(new Font("Arial", Font.BOLD, 15));
 
 		//Agregamos los cammpo para que escriba el contenido
-		JTextField textUrl = new JTextField("http://");
-		JTextField textTitulo = new JTextField("<titulo personalizado>");
-		textUrl.setBounds(80, 50, 280, 25);
-		textTitulo.setBounds(80, 90, 280, 25);
+		entrada[0] = new JTextField("http://");
+		entrada[1] = new JTextField("<titulo personalizado>");
+		entrada[0].setBounds(80, 50, 280, 25);
+		entrada[1].setBounds(80, 90, 280, 25);
+		
+		//habilitamos un escucha a este btn, pero lo haremos aqui mismo para evitar problemas :D
+		boton[0].addActionListener((ActionListener) this);
 		
 		System.out.println("[CajaDialogo][agregarCanal] esperando acciones");
-		this.setTitle("Introdicor valores del nuevo canal");
+		this.setTitle("Introducir valores del nuevo canal");
 		
 		titulo.setText("Datos del nuevo RSS");
-		panel.add(agregar);
+		panel.add(boton[0]);
 		panel.add(titulo);
-		panel.add(lblUrl);
-		panel.add(lblTitulo);
-		panel.add(textTitulo);
-		panel.add(textUrl);
+		panel.add(texto[0]);
+		panel.add(texto[1]);
+		panel.add(entrada[1]);
+		panel.add(entrada[0]);
 				
 		estilizar();
 		this.add(panel);
@@ -71,7 +87,17 @@ public class CajaDialogo extends JFrame{
 	public void estilizar(){
 		panel.setBackground(new Color(33,33,33));
 		panel.setForeground(Color.white);
-		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == boton[0]){
+			System.out.println("[CajaDialogo][BTN] se \"agrego\" un elemento nuevo");
+			if(espera == "addRSS"){
+				//vamos a agregar entonces ::
+			}
+		}
 	}
 	
 }
