ce666a288be81d0982c41a4acab911054b631751
David256 <davidborja456@gmail.com>
2015-10-28 17:01:22 -0500

    se agrego metodos de busqueda por coincidencia

--- src/interfaz/ListaCanales.java
+++ src/interfaz/ListaCanales.java
@@ -6,18 +6,23 @@ import java.util.Vector;
 
 import javax.swing.JList;
 
+import noticias.rss.Canal;
 import Canaletas.Canales;
 
 //Esta es la clase que se encarga de manejar el listado de canales
 
 public class ListaCanales extends JList implements Runnable{
 	
+	private Vector<Canal> interno = new Vector();
+	
 	public ListaCanales(String[] valores){
 		super(valores);
+		estilizar();
+	}
+	public void estilizar(){
 		this.setSize(350, 400);
 		this.setLocation(8, 50);
 		this.setFont(new Font(Font.SANS_SERIF,Font.CENTER_BASELINE , 18));
-		
 		//cambio de color a el JList
 		this.setForeground(new Color(107, 72, 1));
 		this.setBackground(new Color(203, 198, 176));
@@ -27,7 +32,7 @@ public class ListaCanales extends JList implements Runnable{
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
 		while(true){
 			try {
 				Thread.sleep(3000);
@@ -37,13 +42,31 @@ public class ListaCanales extends JList implements Runnable{
 				e.printStackTrace();
 			}
 			//vamos a actualidar eso
-			this.setListData(Canales.getVector().toArray());
+			this.interno = Canales.getVector(); //guardamos el vector dentro del mismo
+			//de esta forma, creo que sería más facil para el actualizador de estado y lista
+			this.setListData(interno.toArray());
 			System.out.println("(Away)[ListaCanales] lanzamiento de thread exitoso");
 		}
 	}
 	public void starter(){
 		new Thread(this).start();
-		
 	}
-
+	
+	//metodos que se encargan de buscar sobre una url o sobre un titulo en particular
+	public String buscarUrl(String titulo){
+		for(int i=0; i<interno.size(); i++){
+			if(interno.get(i).getTitulo() == titulo){
+				return interno.get(i).getUrl();
+			}
+		}
+		return null;
+	}
+	public String buscarTitulo(String url){
+		for(int i=0; i<interno.size(); i++){
+			if(interno.get(i).getUrl() == url){
+				return interno.get(i).getTitulo();
+			}
+		}
+		return null;
+	}
 }
